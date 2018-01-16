00a1114f80a8a16e0efb37291d57dcd1570e3f89
zhenaytnk <tinkalyk@mail.ru>
2018-01-16 23:47:20 +0300

    Подключить WPF Chart. Добавить пример #3

---
+++ parserlog.dotnet.ui/view_model/ChartElement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace parserlog.dotnet.ui.view_model
+{
+    public class ChartElement
+        : Notifier
+    {
+
+        private string _name = string.Empty;
+        private int _count = 0;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                NotifyPropertyChanged("Name");
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+            set
+            {
+                _count = value;
+                NotifyPropertyChanged("Count");
+            }
+        }
+    }
+}
--- parserlog.dotnet.ui/view_model/ViewModel.cs
+++ parserlog.dotnet.ui/view_model/ViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading;
+using System.Collections.ObjectModel;
 
 namespace parserlog.dotnet.ui.view_model
 {
@@ -8,6 +9,12 @@ namespace parserlog.dotnet.ui.view_model
         public ViewModel()
         {
             OpenFileCommand = new command.OpenFileCommand(this);
+            data_chart = new ObservableCollection<ChartElement>();
+            data_chart.Add(new ChartElement() { Name = "China", Count = 1340 });
+            data_chart.Add(new ChartElement() { Name = "India", Count = 1220 });
+            data_chart.Add(new ChartElement() { Name = "United States", Count = 309 });
+            data_chart.Add(new ChartElement() { Name = "Indonesia", Count = 240 });
+
         }
 
         public string LogName
@@ -22,9 +29,18 @@ namespace parserlog.dotnet.ui.view_model
                 NotifyPropertyChanged("LogName");
             }
         }
-        
-        public command.OpenFileCommand OpenFileCommand { get; set; }
 
+        public command.OpenFileCommand OpenFileCommand { get; set; }
+        public ObservableCollection<ChartElement> DataChart
+        {
+            get
+            {
+                return data_chart;
+            }
+        }
+
+
+        private readonly ObservableCollection<ChartElement> data_chart;
         private string log_name;
     }
 }
